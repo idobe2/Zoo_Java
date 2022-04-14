4109b47fb1637baa36126df8fd7353bc0d4ea8df
idobe2 <idobenoun@gmail.com>
2022-04-14 20:43:53 +0300

    AddAnimalDialog is updated.

--- graphics/AddAnimalDialog.java
+++ graphics/AddAnimalDialog.java
@@ -6,34 +6,53 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddAnimalDialog extends JDialog {
-    JDialog d = new JDialog();
-    JFrame f = new JFrame("Add Animal");
+    private final InputsPanel inputs;
+    //private final JDialog d = new JDialog();
+    private final JFrame f = new JFrame("Add Animal");
 
-    AddAnimalDialog() {
-        final JLabel label = new JLabel();
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setSize(400, 100);
-        JButton b = new JButton("Show");
-        b.setBounds(200, 100, 75, 20);
-        String languages[] = {"Elephant", "Lion", "Giraffe", "Turtle", "Bear"};
-        final JComboBox cb = new JComboBox(languages);
-        cb.setBounds(50, 100, 90, 20);
-        this.add(cb);
-        this.add(label);
-        this.add(b);
-        this.setLayout(null);
-        this.setSize(350, 350);
-        this.setVisible(true);
-        b.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String data = "Animal Selected: "
-                        + cb.getItemAt(cb.getSelectedIndex());
-                label.setText(data);
-                //System.out.println(cb.getItemAt(cb.getSelectedIndex())); // String of animal
-            }
-        });
+    private static class InputsPanel extends JPanel {
+        private final JTextField tbSize, tbHspeed, tbVspeed;
+
+        public InputsPanel() {
+            this.setLayout(new GridLayout(0, 2));
+            this.add(new JLabel("Size:"));
+            this.add(tbSize = new JTextField());
+            this.add(new JLabel("Horizontal speed:"));
+            this.add(tbHspeed = new JTextField());
+            this.add(new JLabel("Vertical speed:"));
+            this.add(tbVspeed = new JTextField());
+        }
     }
-}
+
+    public AddAnimalDialog() {
+            final JLabel label = new JLabel();
+            label.setHorizontalAlignment(JLabel.CENTER);
+            label.setSize(400, 100);
+            JButton b = new JButton("Show");
+            b.setBounds(200, 100, 75, 20);
+            String animals[] = {"Elephant", "Lion", "Giraffe", "Turtle", "Bear"};
+            final JComboBox cb = new JComboBox(animals);
+            cb.setBounds(50, 100, 90, 20);
+            this.add(cb);
+            this.add(label);
+            this.add(b);
+            this.add(inputs = new InputsPanel());
+            //this.add(new CalculatePanel());
+            //this.setLayout(null);
+            this.setSize(350, 350);
+            //this.pack();
+            this.setVisible(true);
+            b.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String data = "Animal Selected: "
+                            + cb.getItemAt(cb.getSelectedIndex());
+                    label.setText(data);
+                    //System.out.println(cb.getItemAt(cb.getSelectedIndex())); // String of animal
+                }
+            });
+        }
+    }
+
 //    AddAnimalDialog() {
 //        d.setLayout( new FlowLayout() );
 //        JCheckBox checkBoxElephant = new JCheckBox("Elephant");
