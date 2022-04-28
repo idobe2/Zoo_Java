3e974f3c2f3fea141531736775d8b60b5cf39419
idobe2 <idobenoun@gmail.com>
2022-04-28 18:16:13 +0300

    Added ActionListener and ActionEvent to food button and for each food type.

--- graphics/ZooPanel.java
+++ graphics/ZooPanel.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import static javax.swing.GroupLayout.Alignment.*;
+
 public class ZooPanel extends JPanel { // implements Runnable // public void run() {}
     ArrayList<Animal> animalArrayList = new ArrayList<>();
 
@@ -14,6 +16,8 @@ public class ZooPanel extends JPanel { // implements Runnable // public void run
         this.setLayout(new FlowLayout(FlowLayout.CENTER));
         JButton addAnimalButton = new JButton("Add Animal");
         JButton moveAnimalButton = new JButton("Move Animal");
+        JButton clearButton = new JButton("Clear");
+        JButton foodButton = new JButton("Food");
         JButton exitButton = new JButton("Exit");
         this.add(addAnimalButton);
         addAnimalButton.addActionListener(new ActionListener() {
@@ -33,8 +37,57 @@ public class ZooPanel extends JPanel { // implements Runnable // public void run
                 new MoveAnimalDialog(animalArrayList);
             }
         });
-        this.add(new JButton("Clear"));
-        this.add(new JButton("Food"));
+        this.add(clearButton);
+        this.add(foodButton);
+        foodButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Test");
+                JFrame foodFrame = new JFrame("Food for animals");
+                //foodFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                Container myPanel = foodFrame.getContentPane();
+                GroupLayout groupLayout = new GroupLayout(myPanel);
+                groupLayout.setAutoCreateGaps(true);
+                groupLayout.setAutoCreateContainerGaps(true);
+                myPanel.setLayout(groupLayout);
+                JLabel lF = new JLabel("Please choose food");
+                lF.setSize(400, 200);
+                lF.setHorizontalAlignment(JLabel.CENTER);
+                foodFrame.add(lF);
+                JButton b1 = new JButton("Lettuce");
+                JButton b2 = new JButton("Cabbage");
+                JButton b3 = new JButton("Meat");
+
+                groupLayout.setHorizontalGroup(groupLayout.createSequentialGroup()
+                        .addGroup(groupLayout.createParallelGroup(LEADING).addComponent(b1).addComponent(b3))
+                        .addGroup(groupLayout.createParallelGroup(TRAILING).addComponent(b2)));
+
+                groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+                        .addGroup(groupLayout.createParallelGroup(BASELINE).addComponent(b1).addComponent(b2))
+                        .addGroup(groupLayout.createParallelGroup(BASELINE).addComponent(b3)));
+
+                foodFrame.setSize(400,200);
+                foodFrame.setVisible(true);
+                b1.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Lettuce");
+                    }
+                });
+                b2.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Cabbage");
+                    }
+                });
+                b3.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Meat");
+                    }
+                });
+            }
+        });
         this.add(new JButton("Info"));
         this.add(exitButton);
         exitButton.addActionListener(new ActionListener() {
